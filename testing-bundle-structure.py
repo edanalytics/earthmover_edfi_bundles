b12545b7f9e99168ec34c1b4ac3738fc5c2dba58
ayurkovskaya <syurkovskaya@mail.wlu.edu>
2025-08-08 16:31:46 -0500

    Final commit before I leave. This is still a mess. Have fun!!

--- testing-bundle-structure.py
+++ testing-bundle-structure.py
@@ -18,17 +18,16 @@ templates/studentAssessment.jsont
 
 """
 
+# TODO: defining the tests
+
+FILES = "Bundle Folder Contents"
+PARAMS = "Getting EM Parameters"
+COMPL = "Compiling earthmover.yaml"
+IDPAC = "ID Package Compatibility"
 
 
-# FIXME: bad bundles: ACCUPLACER and CogAT
+# TODO: exceptions for the testing 
 
-
-
-
-
-# TODO: exceptions for the testing # FIXME: idk if I want this
-
-class NoReadme(Exception): pass
 class NoEMParams(Exception): pass
 class InvalidBlockError(Exception): pass
 
@@ -38,15 +37,13 @@ class InvalidBlockError(Exception): pass
 class Checker:
     def __init__(self, folder_path):
         self.folder_path = folder_path
-        self.warnings = {"Bundle Folder Contents": [], "Getting EM Parameters": [], "Compiling earthmover.yaml": [], "ID Package Compatibility": []}
-        self.successes = {key: [] for key in self.warnings}
 
+        self.all_tests = [FILES, PARAMS, COMPL, IDPAC]
 
-        self.not_run_tests = \
-            ["Bundle Folder Contents",
-            "Getting EM Parameters",
-            "Compiling earthmover.yaml",
-            "ID Package Compatibility"] # FIXME: add more tests
+        self.warnings = {test: [] for test in self.all_tests}
+        self.successes = {test: [] for test in self.all_tests}
+
+        self.not_run_tests = list(self.not_run_tests)
 
 
     # TODO: checking folder contents 
@@ -58,28 +55,27 @@ class Checker:
             'earthmover.yaml', 'README.md', 
             'data', '_metadata.yaml', 'seeds']
 
-        self.has_readme = 'README.md' not in folder_contents
-        self.has_metadata = '_metadata.yaml' not in folder_contents
-
 
         if set(required_contents) - set(folder_contents) == set():
-            self.successes["Bundle Folder Contents"].append("You seem to have all the necessary files and folders at the root.\n          "+ 
+            self.successes[FILES].append("You seem to have all the necessary files and folders at the root.\n          "+ 
                                                             "\n          ".join(f"{item}" for item in required_contents)+"\n")
         else:
             missing_contents = list(set(required_contents) - set(folder_contents))
-            self.warnings["Bundle Folder Contents"].append(f"You are missing the following files/folders:\n          "+
+            self.warnings[FILES].append(f"You are missing the following files/folders:\n          "+
                                                            "\n          ".join(f"{item}" for item in missing_contents)+"\n")  
 
+        n_em_yaml = sum("earthmover" in f and f.endswith(".yaml") for f in folder_contents)
+        n_lb_yaml = sum("lightbeam" in f and f.endswith(".yaml") for f in folder_contents)
 
-        if folder_contents.count("earthmover.yaml") > 1:
-            self.warnings["Bundle Folder Contents"].append(f"Please make sure there is one earthmover.yaml file in your bundle, found {folder_contents.count('earthmover.yaml')}")
+        if n_em_yaml > 1:
+            self.warnings[FILES].append(f"Please make sure there is one earthmover config file in your bundle, found {n_em_yaml}")
         else:
-            self.successes["Bundle Folder Contents"].append("There is a single earthmover.yaml file")
+            self.successes[FILES].append("There is a single earthmover config file")
 
-        if folder_contents.count("lightbeam.yaml") > 1:
-            self.warnings["Bundle Folder Contents"].append(f"Please make sure there is one lightbeam.yaml file in your bundle, found {folder_contents.count('lightbeam.yaml')}")
+        if n_lb_yaml > 1:
+            self.warnings[FILES].append(f"Please make sure there is one lightbeam config file in your bundle, found {n_lb_yaml}")
         else:
-            self.successes["Bundle Folder Contents"].append("There is a single lightbeam.yaml file")
+            self.successes[FILES].append("There is a single lightbeam config file")
 
 
         self.has_sample_data = False
@@ -90,14 +86,14 @@ class Checker:
 
         if data_folder:
             self.has_sample_data = True
-            self.successes["Bundle Folder Contents"].append("Found the following sample data files:\n          " +
+            self.successes[FILES].append("Found the following sample data files:\n          " +
                                                             "\n          ".join(file for file in data_folder))
         
         if not self.has_sample_data:
-            self.warnings["Bundle Folder Contents"].append("Missing a sample data file")
+            self.warnings[FILES].append("Missing a sample data file")
 
 
-        self.not_run_tests.remove("Bundle Folder Contents")
+        self.not_run_tests.remove(FILES)
 
 
     # TODO: parsing readme 
@@ -108,21 +104,18 @@ class Checker:
 
         readme_file_path = self.folder_path + "/README.md"
 
-        try:
-            with open(readme_file_path, "r", encoding="utf-8") as f:
+        with open(readme_file_path, "r", encoding="utf-8") as f:
                 readme_file = f.read()
-        except FileNotFoundError:
-            raise NoReadme
 
         for pattern in ["./earthmover.yaml", "earthmover.yaml"]:
             try:
                 params_string = readme_file.split(f"earthmover run -c {pattern} -p '")[1].split("'\n```")[0]
-                self.successes["Getting EM Parameters"].append("Found sample Earthmover run command in the README.md")
+                self.successes[PARAMS].append("Found sample Earthmover run command in the README.md")
                 break
             except IndexError:
                 continue
         else:
-            self.warnings["Getting EM Parameters"].append("Please make sure there is a sample Earthmover run command in README.md")
+            self.warnings[PARAMS].append("Please make sure there is a sample Earthmover run command in README.md")
 
 
 
@@ -130,11 +123,11 @@ class Checker:
         try:
             em_params_dict = json.loads(params_string)
             self.em_params = json.dumps(em_params_dict)
-            self.successes["Getting EM Parameters"].append("The Earthmover command is valid JSON")
+            self.successes[PARAMS].append("The Earthmover command is valid JSON")
         except json.JSONDecodeError:
-            self.warnings["Getting EM Parameters"].append("Unable to use the provided earthmover command, invalid JSON: ", params_string) 
+            self.warnings[PARAMS].append("Unable to use the provided earthmover command, invalid JSON: ", params_string) 
 
-        self.not_run_tests.remove("Getting EM Parameters")
+        self.not_run_tests.remove(PARAMS)
 
 
 
@@ -149,12 +142,10 @@ class Checker:
         self.em_yaml_path = self.folder_path + "/earthmover.yaml"
 
         command = f'''
-        source ~/.venv/earthmover/bin/activate && \
         earthmover compile -c {self.em_yaml_path} -p '{self.em_params}' 
         '''
         #                                                  && \
         # earthmover run -c {self.em_yaml_path} -p '{params}'
-
 
         result = subprocess.run(
             ["bash", "-c", command],
@@ -162,83 +153,11 @@ class Checker:
             text=True)
 
         if result.returncode == 0:
-            self.successes["Compiling earthmover.yaml"].append("Successfully compiled with Earthmover")
+            self.successes[COMPL].append("Successfully compiled with Earthmover")
         else:
-            self.warnings["Compiling earthmover.yaml"].append(f"Error during compiling with Earthmover:\n{result.stdout}\n{result.stderr}")
+            self.warnings[COMPL].append(f"Error during compiling with Earthmover:\n{result.stdout}\n{result.stderr}")
 
-        self.not_run_tests.remove("Compiling earthmover.yaml")
-
-
-
-    # TODO: studentAssessment.jsont 
-    def something_studentAssessment(self):
-    # FIXME: useless, not running it
-    # FIXME: I don't know what to do with this
-
-        templ_st_assess_path = f"{self.folder_path}/templates/studentAssessment.jsont"
-
-        try:
-            with open(templ_st_assess_path, "r") as f:
-                self.templ_st_assess = f.read()
-
-        except FileNotFoundError:
-            templ_st_assess_path = f"{self.folder_path}/templates/studentAssessments.jsont"
-            with open(templ_st_assess_path, "r") as f:
-                self.templ_st_assess = f.read()
-
-        except FileNotFoundError:
-            self.warnings.append("Please make sure there is studentAssessments.jsont file in templates")
-
-
-    """
-    We need something reallyyyyy close to this
-
-
-
-
-
-    from PSAT_SAT
-
-    same code in {"studentObjectiveAssessments": [...{"score_results": [....
-            and in {"scoreResults": [....
-
-    74 -----
-    {%- for score in all_scores -%}
-        {
-                "assessmentReportingMethodDescriptor": "{{namespace}}/AssessmentReportingMethodDescriptor#{{score[0]}}",
-                "resultDatatypeTypeDescriptor": "uri://ed-fi.org/ResultDatatypeTypeDescriptor#Integer",
-                "result": "{{score[1]}}"
-        }
-        {%- if not loop.last -%},
-        {%- else %}{% endif %}{%- endfor -%}
-        ],
-
-    177 -------
-        "scoreResults": [
-        {%- for score in all_score_obj -%}
-        {
-        "assessmentReportingMethodDescriptor": "{{namespace}}/AssessmentReportingMethodDescriptor#{{score[0]}}",
-        "resultDatatypeTypeDescriptor": "uri://ed-fi.org/ResultDatatypeTypeDescriptor#Integer",
-        "result": "{{score[1]}}"
-        }
-        {%- if not loop.last -%},
-        {%- else %}{% endif %}{%- endfor -%}
-    ]
-
-
-
-    """
-
-
-    # check for the templates NOT hard-coding descriptor namespaces
-    # -> under templates
-    # -> parse jsont
-            # bottom of page 2
-
-
-
-    # check json files to make sure they are valid
-    # -> commas, formatting
+        self.not_run_tests.remove(COMPL)
 
 
 
@@ -257,11 +176,11 @@ class Checker:
 
         try:
             if self.compiled_em_yaml_text["transformations"]["input"] == {'source': '$sources.input', 'operations': []}:
-                self.successes["ID Package Compatibility"].append("Correct transformations.input")
+                self.successes[IDPAC].append("Correct transformations.input")
             else:
                 raise InvalidBlockError
         except (KeyError, InvalidBlockError):
-            self.warnings["ID Package Compatibility"].append("Please make sure the following block is in your `earthmover.yaml`:\n\n"
+            self.warnings[IDPAC].append("Please make sure the following block is in your `earthmover.yaml`:\n\n"
                                                                 "transformations:\n"
                                                                 "  input:\n"
                                                                 "    source: '$sources.input'\n"
@@ -270,23 +189,25 @@ class Checker:
 
         try:
             if self.compiled_em_yaml_text["config"]["parameter_defaults"]["STUDENT_ID_NAME"] == "edFi_studentUniqueID":
-                self.successes["ID Package Compatibility"].append("Correct config.parameter_defaults.STUDENT_ID_NAME")
+                self.successes[IDPAC].append("Correct config.parameter_defaults.STUDENT_ID_NAME")
             else:
                 raise InvalidBlockError
         except (KeyError, InvalidBlockError):
-            self.warnings["ID Package Compatibility"].append("Please make sure the following block is in your `earthmover.yaml`:\n\n"
+            self.warnings[IDPAC].append("Please make sure the following block is in your `earthmover.yaml`:\n\n"
                                                                 "config:\n"
                                                                 "  parameter_defaults:\n"
                                                                 '    STUDENT_ID_NAME: "edFi_studentUniqueID"')
                 
 
-        self.not_run_tests.remove("ID Package Compatibility")
+        self.not_run_tests.remove(IDPAC)
 
 
     # TODO: exit testing
     def exit_testing(self):
 
         """✨ ⚠️ ✅"""
+
+        exit_code = 0
 
 
         for key in self.warnings.keys():
@@ -332,9 +253,7 @@ def main():
         except NoEMParams:
             pass
 
-        # c.something_studentAssessment() # FIXME: this does nothing rn
-
-    except NoReadme:
+    except FileNotFoundError:
         pass
 
     finally:
@@ -345,36 +264,75 @@ def main():
 
 
 
-
-
 if __name__ == "__main__":
     main()
 
 
 
+"""
+    # TODO: this code parses the studentAssessment(s) file, doesn't # FIXME: say more
+    def something_studentAssessment(self):
+
+        templ_st_assess_path = f"{self.folder_path}/templates/studentAssessment.jsont"
+
+        try:
+            with open(templ_st_assess_path, "r") as f:
+                self.templ_st_assess = f.read()
+
+        except FileNotFoundError:
+            templ_st_assess_path = f"{self.folder_path}/templates/studentAssessments.jsont"
+            with open(templ_st_assess_path, "r") as f:
+                self.templ_st_assess = f.read()
+
+        except FileNotFoundError:
+            self.warnings.append("Please make sure there is studentAssessments.jsont file in templates")
+
+"""
+
 
 
 """
-#     # QUESTIONS #
+    We need something reallyyyyy close to this
 
-# # how do I know if the assessment is local (eg state-specific)
+    from PSAT_SAT
 
-"""
+    same code in {"studentObjectiveAssessments": [...{"score_results": [....
+            and in {"scoreResults": [....
+
+    74 -----
+    {%- for score in all_scores -%}
+        {
+                "assessmentReportingMethodDescriptor": "{{namespace}}/AssessmentReportingMethodDescriptor#{{score[0]}}",
+                "resultDatatypeTypeDescriptor": "uri://ed-fi.org/ResultDatatypeTypeDescriptor#Integer",
+                "result": "{{score[1]}}"
+        }
+        {%- if not loop.last -%},
+        {%- else %}{% endif %}{%- endfor -%}
+        ],
+
+    177 -------
+        "scoreResults": [
+        {%- for score in all_score_obj -%}
+        {
+        "assessmentReportingMethodDescriptor": "{{namespace}}/AssessmentReportingMethodDescriptor#{{score[0]}}",
+        "resultDatatypeTypeDescriptor": "uri://ed-fi.org/ResultDatatypeTypeDescriptor#Integer",
+        "result": "{{score[1]}}"
+        }
+        {%- if not loop.last -%},
+        {%- else %}{% endif %}{%- endfor -%}
+    ]
 
 
 
+    """
+
+
+    # check for the templates NOT hard-coding descriptor namespaces
+    # -> under templates
+    # -> parse jsont
+            # bottom of page 2
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+    # check json files to make sure they are valid
+    # -> commas, formatting
